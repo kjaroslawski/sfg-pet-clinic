b9bcf0f214ccb0a455237390e15d74b34c774bfa
Konrad Jarosławski <kgj.mails@gmail.com>
2018-12-17 00:25:54 +0100

    Implement Interfaces for Services. Closes #5.

---
+++ pet-clinic-data/src/main/java/pl/qamar/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package pl.qamar.sfgpetclinic.services;
+
+import pl.qamar.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/pl/qamar/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package pl.qamar.sfgpetclinic.services;
+
+import pl.qamar.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
