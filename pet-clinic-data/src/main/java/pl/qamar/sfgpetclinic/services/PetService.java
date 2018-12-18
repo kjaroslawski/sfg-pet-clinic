35dc8e1a8cc63c975e7fea8da97d18d58f73e0af
Konrad Jarosławski <kgj.mails@gmail.com>
2018-12-18 21:42:45 +0100

    Refactoring to common CRUD interface. Closes #11.

--- pet-clinic-data/src/main/java/pl/qamar/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/pl/qamar/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package pl.qamar.sfgpetclinic.services;
 
 import pl.qamar.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/pl/qamar/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/pl/qamar/sfgpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package pl.qamar.sfgpetclinic.services;
 
 import pl.qamar.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
